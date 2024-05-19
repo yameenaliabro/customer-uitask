4a56ac921f4c516d280810782ad7ce28f2325ba8
Muhammad Yameen <106884997+yameenaliabro@users.noreply.github.com>
2024-05-20 00:52:13 +0500

    Update DeleteModal.tsx

--- src/components/Modal/DeleteModal.tsx
+++ src/components/Modal/DeleteModal.tsx
@@ -21,7 +21,7 @@ function DeleteModal(props: deleteModalProps) {
                 <h2 className='font-semibold text-[30px] text-[#000000]'>Are you sure?</h2>
                 <p className='font-medium text-[24px] text-center items-center flex flex-col'>Do you really want to delete this customer? This process can not be undone.</p>
                 <div className='flex flex-row justify-around w-full  mt-[40px]'>
-                    <button className='text-white px-[60px] cancel-button py-[8px] rounded-md text-lg font-semibold' onClick={onDelete}>Cancel</button>
+                    <button className='text-white px-[60px] cancel-button py-[8px] rounded-md text-lg font-semibold' onClick={onClose}>Cancel</button>
                     <button onClick={deleteCustomer} className='text-white px-[60px] py-[8px] delete-button  rounded-md text-lg font-semibold'>Delete</button>
                 </div>
             </div>
